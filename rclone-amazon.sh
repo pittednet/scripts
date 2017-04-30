85e24e52a422c52ed974e8b1153c660e9acb3ee2
pittednet <github@pitted.net>
2017-04-30 16:43:59 +0100

    removed write to desktop notification file

--- rclone-amazon.sh
+++ rclone-amazon.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-## (date && rclone --filter-from rclone-filter.txt sync /Volumes/Movies\ \&\ Photos/media amazon: -v) 2>> rclone-amazon.log
-
-#!/bin/sh
 home="/Users/richard" ## note -- this will be run as root, ~ is not your normal user
-now=$(date "+%Y-%m-%d %H.%M.%S")
-echo $now >> "$home/Desktop/TestFile.txt"
 date
-/usr/local/bin/rclone --config "/Users/richard/.config/rclone/rclone.conf" --filter-from "$home/Documents/dev/scripts/rclone-filter.txt" sync /Volumes/Movies\ \&\ Photos/media amazon: -v
+/usr/local/bin/rclone --config "$home/.config/rclone/rclone.conf" --filter-from "$home/Documents/dev/scripts/rclone-filter.txt" sync /Volumes/Movies\ \&\ Photos/media amazon: -v
